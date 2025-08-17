ae06dc64f56abf519a044c710161c0f146920553
Ibrahim-emad92 <ibrahimemad953@yahoo.com>
2025-08-17 23:05:54 +0300

    crud in categor

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -24,7 +24,80 @@ const createCategoryController=async(req,res)=>{
         })
     }
 }
-    
+const getAllCategoryController=async(req,res)=>{
+    try {
+        const categories=await categoryModel.find({});
+        if(!categories){
+            res.status(404).send({
+            success:false,
+            massage:'Not find Categories'
+            })
+        }  
+        res.status(200).send({
+            success:true,
+            categories
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            massage:'Error in Api',
+            error
+        })
+    }
+}
+
+const updateCategoryByIdController=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const {title,imageUrl}=req.body;
+        const updateCategory=await categoryModel.findByIdAndUpdate(id,{title,imageUrl},{new:true});
+
+        if(!updateCategory){
+            res.status(404).send({
+            success:false,
+            massage:'Category Not founded'
+            })
+        }
+        res.status(200).send({
+            success:true,
+            massage:'category updated',
+            updateCategory
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            massage:'Error in Api',
+            error
+        })
+    }
+}
+const deleteCategoryController=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        const category=await categoryModel.findById(id);
+        if(!category){
+            res.status(404).send({
+            success:false,
+            massage:'Category Not founded'
+            })
+        }
+        await categoryModel.findByIdAndDelete(id);
+        res.status(200).send({
+            success:true,
+            massage:'category deleted'
+        })
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            massage:'Error in Api',
+            error
+        })
+    }
+}
+
 module.exports={
-    createCategoryController
+    createCategoryController,
+    getAllCategoryController,
+    updateCategoryByIdController,
+    deleteCategoryController
 }
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,8 +1,12 @@
 const express=require('express');
 const router = express.Router();
-const {createCategoryController}=require('../controllers/categoryController');
+const {createCategoryController,getAllCategoryController,updateCategoryByIdController,deleteCategoryController}=require('../controllers/categoryController');
+const authMiddleware=require('../middlewares/authMiddlewares')
 
 
-router.post('/create',createCategoryController);
+router.post('/create',authMiddleware,createCategoryController);
+router.get('/getAll',getAllCategoryController);
+router.put('/update/:id',authMiddleware,updateCategoryByIdController);
+router.delete('/delete/:id',authMiddleware,deleteCategoryController);
 
 module.exports=router
